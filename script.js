de7d047abf5aeecd80b8a94ff4d771edf152e1fb
Sachin Nagila <sachinnagila2053@gmail.com>
2024-05-16 12:06:57 +1000

    stopwatch with css and start button working

---
+++ script.js
@@ -0,0 +1,62 @@
+let minutes = 0;
+let seconds = 0;
+let tens = 00;
+let appendMinutes = document.querySelector("#minutes");
+let appendSeconds = document.querySelector("#seconds");
+let appendTens = document.querySelector("#mSeconds");
+
+let startBtn = document.querySelector("#start");
+let stopBtn = document.querySelector("#stop");
+let resetBtn = document.querySelector("#reset");
+
+const startTimer = () => {
+  tens++;
+  if (tens <= 9) {
+    appendTens.innerHTML = "0" + tens;
+  }
+  if (tens > 9) {
+    appendTens.innerHTML = tens;
+  }
+  if (tens > 99) {
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + tens;
+  }
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+  if (seconds > 59) {
+    minutes++;
+    appendMinutes.innerHTML = "0" + minutes;
+    seconds = 0;
+    appendSeconds.innerHTML = "0" + seconds;
+  }
+  if (minutes <= 9) {
+    appendMinutes.innerHTML = "0" + minutes;
+  }
+  if (minutes > 9) {
+    appendMinutes.innerHTML = minutes;
+  }
+  if (minutes > 59) {
+    appendTens.innerHTML = "0" + tens;
+    appendSeconds.innerHTML = "0" + seconds;
+    appendMinutes.innerHTML = "0" + minutes;
+  }
+};
+
+// let start = startBtn.addEventListener("click", () => {
+//   interval = setInterval(startTimer, 10);
+// });
+// stopBtn.addEventListener("click", () => {
+//   console.log("hey");
+//   clearInterval(start);
+// });
+
+startBtn.onclick = (interval) => {
+  interval = setInterval(startTimer, 10);
+};
+
+stopBtn.onclick = () => {
+  clearInterval(interval);
+};
